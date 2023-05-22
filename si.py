a007949dae57f41e97ab5aee578888a056ea2a26
nicopx2468 <134159569+nicopx2468@users.noreply.github.com>
2023-05-22 21:06:25 +0200

    Update and rename README.md to si.py

---
+++ si.py
@@ -0,0 +1,5 @@
+#Programa que pide al usuario un numero entero y muestre por pantalla un triángulo rectángulo como el de abajo, del número introducido la#altura
+#Vera Ruiz
+num = eval(input("Dime un numero entero:"))
+for i in range(0,num+1):
+    print(1*"*")
